8003f68c001b8b1d105e740f3d64a3b85892d602
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-12-01 20:30:28 +0300

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,24 @@
+﻿using namespace std;
+#include <iostream>
+struct gnom {
+    int voz;
+    int ros;
+    char p;
+};
+
+int main()
+{
+    gnom a;
+    a.voz = 10;
+    a.ros = 134;
+    a.p = 'W';
+    gnom b;
+    b.voz = 38;
+    b.ros = 68;
+    b.p = 'M';
+    gnom c;
+    c.voz = 38;
+    c.ros = 96;
+    c.p = 'M';
+    cout << c.ros + b.ros + a.ros;
+}
